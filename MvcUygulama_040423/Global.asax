6a9c424e37e03036551477c58907875b2262b96a
umeyr13 <umeyr.gulcimen@gmail.com>
2023-04-04 14:51:50 +0300

    Add project files.

---
+++ MvcUygulama_040423/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcUygulama_040423.MvcApplication" Language="C#" %>
---
+++ MvcUygulama_040423/Library/My_Htnl_Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcUygulama_040423.Library
+{
+    public static class My_Htnl_Helper
+    {
+        public static MvcHtmlString Alert(this HtmlHelper helper, string id="alert1",string renk="success", string text="")
+        {
+
+            TagBuilder tag = new TagBuilder("div");
+            tag.AddCssClass("alert alert-"+renk);
+            tag.GenerateId(id);
+            tag.Attributes.Add(new KeyValuePair<string,string>("role", "alert1"));
+            tag.SetInnerText(text);
+            return MvcHtmlString.Create(tag.ToString());    
+
+        }
+
+    }
+}
